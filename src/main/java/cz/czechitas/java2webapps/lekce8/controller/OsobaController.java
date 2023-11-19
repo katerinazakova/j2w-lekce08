398117fa61512a6b26c847b36e73729f850ed906
Katerina Zakova <zakova.kz@gmail.com>
2023-11-19 17:16:37 +0100

    Feat: create repository

--- src/main/java/cz/czechitas/java2webapps/lekce8/controller/OsobaController.java
+++ src/main/java/cz/czechitas/java2webapps/lekce8/controller/OsobaController.java
@@ -1,19 +1,29 @@
 package cz.czechitas.java2webapps.lekce8.controller;
 
 import cz.czechitas.java2webapps.lekce8.entity.Osoba;
+import cz.czechitas.java2webapps.lekce8.repository.OsobaRepository;
 import jakarta.validation.Valid;
 import org.springframework.beans.propertyeditors.StringTrimmerEditor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.time.LocalDate;
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 public class OsobaController {
+private final OsobaRepository osobaRepository;
+
+  public OsobaController(OsobaRepository osobaRepository) {
+    this.osobaRepository = osobaRepository;
+  }
 
   private final List<Osoba> seznamOsob = List.of(
           new Osoba(1L, "Božena", "Němcová", LocalDate.of(1820, 2, 4), "Vídeň", null, null)
@@ -29,7 +39,7 @@ public class OsobaController {
   public ModelAndView seznam() {
     //TODO načíst seznam osob
     return new ModelAndView("seznam")
-            .addObject("osoby", seznamOsob);
+            .addObject("osoby", osobaRepository.findAll());
   }
 
   @GetMapping("/novy")
@@ -43,6 +53,8 @@ public class OsobaController {
     if (bindingResult.hasErrors()) {
       return "detail";
     }
+    osoba.setId(null);
+    osobaRepository.save(osoba);
     //TODO uložit údaj o nové osobě
     return "redirect:/";
   }
@@ -50,22 +62,34 @@ public class OsobaController {
   @GetMapping("/{id:[0-9]+}")
   public ModelAndView detail(@PathVariable long id) {
     //TODO načíst údaj o osobě
+
+   Optional<Osoba> osoba = osobaRepository.findById(id);
+                  osoba.orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
     return new ModelAndView("detail")
-            .addObject("osoba", seznamOsob.get(0));
+            .addObject("osoba", osoba.get());
   }
 
+    /*
+    Osoba nalezenaOsoba = osobaRepository.findById(id)
+            .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+    return new ModelAndView("detail")
+            .addObject("osoba", nalezenaOsoba);
+  }
+*/
   @PostMapping("/{id:[0-9]+}")
-  public String ulozit(@PathVariable long id, @ModelAttribute("osoba") @Valid Osoba osoba, BindingResult bindingResult) {
+  public String ulozit(@ModelAttribute("osoba") @Valid Osoba osoba, BindingResult bindingResult) {
     if (bindingResult.hasErrors()) {
       return "detail";
     }
     //TODO uložit údaj o osobě
+    osobaRepository.save(osoba);
     return "redirect:/";
   }
 
   @PostMapping(value = "/{id:[0-9]+}", params = "akce=smazat")
   public String smazat(@PathVariable long id) {
     //TODO smazat údaj o osobě
+    osobaRepository.deleteById(id);
     return "redirect:/";
   }
 
